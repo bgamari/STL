a4d3e5ef54c280d36e6d2ad21409f1617db659d7
Ben Gamari <ben@smart-cactus.org>
2025-01-26 15:51:01 -0500

    Provide universal parser

--- src/Graphics/Formats/STL.hs
+++ src/Graphics/Formats/STL.hs
@@ -1,10 +1,37 @@
-module Graphics.Formats.STL (
-      textSTL
-    , stlParser
-    , STL(..), Triangle(..), Vector
-    )
-    where
+module Graphics.Formats.STL
+    ( STL(..), Triangle(..), Vector
+      -- * Parsing
+    , parseSTL
+    , parseASCII
+    , parseBinary
+      -- * Printing
+    , toASCII
+    , toBinary
+    ) where
 
-import Graphics.Formats.STL.Printer (textSTL)
-import Graphics.Formats.STL.Parser (stlParser)
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.Text.Encoding as TE
+
 import Graphics.Formats.STL.Types
+import qualified Graphics.Formats.STL.ASCII as ASCII
+import qualified Graphics.Formats.STL.Binary as Binary
+
+-- | Parse an STL file of either format.
+parseSTL :: BSL.ByteString -> Either String STL
+parseSTL bs
+  | magic `BSL.isPrefixOf` bs = parseASCII bs
+  | otherwise                 = parseBinary bs
+  where
+    magic = BSL.pack "solid "
+
+parseBinary :: BSL.ByteString -> Either String STL
+parseBinary = Binary.parse
+
+parseASCII :: BSL.ByteString -> Either String STL
+parseASCII = ASCII.parse . TE.decodeASCII . BSL.toStrict
+
+toASCII :: STL -> BSL.ByteString
+toASCII = ASCII.toSTL
+
+toBinary :: STL -> BSL.ByteString
+toBinary = Binary.toSTL
